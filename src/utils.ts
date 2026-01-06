d5c2f875e04767a234fd7b2be14247be2f6fabfb
Mathijs van de Venne <mathijs@tenpitch.com>
2026-01-06 14:25:14 +0100

    Use shims as electron can't build with import.meta in cjs code

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
 import util from 'node:util';
 
 export function parseNumber(value: string): number | undefined {
@@ -15,12 +13,4 @@ export function parseNumber(value: string): number | undefined {
 export function inspect(data: unknown): void {
     // eslint-disable-next-line
     console.log(util.inspect(data, {showHidden: false, depth: null, colors: true}));
-}
-
-export function getDirname(): string {
-    if (typeof import.meta.url !== 'undefined') {
-        return path.dirname(fileURLToPath(import.meta.url));
-    } else {
-        return __dirname;
-    }
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -9,4 +9,5 @@ export default defineConfig({
     minify: false,
     bundle: true,
     clean: true,
+    shims: true,
 });
